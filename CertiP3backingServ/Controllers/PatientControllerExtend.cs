aabe596d29cdb681ddd237588f5cde09eff989cc
kukinisVO <ronaldofernandez1@upb.edu>
2025-05-11 00:16:01 -0400

    base proyect

---
+++ CertiP3backingServ/Controllers/PatientControllerExtend.cs
@@ -0,0 +1,15 @@
+﻿using CertiP3backingServ.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CertiP3backingServ.Controllers
+{
+    public class PatientControllerExtend : Controller
+    {
+        [HttpPost("api/patientcode")]
+        public IActionResult CalculateCode([FromBody]Patient input)
+        {
+            string code = $"{input.Name[0]}{input.LastName[0]}-{input.CI}";
+            return Ok(code);
+        }
+    }
+}
