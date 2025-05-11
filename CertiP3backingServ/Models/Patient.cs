252d8e724f0b8c0f2fc4a44f32edf015e5f23b2f
Andrew Fernando Alvarez Jordan <165936895+AndrewJordan12@users.noreply.github.com>
2025-05-11 01:54:16 -0400

    andrew: Deleted bloodtype as it isnt used

--- CertiP3backingServ/Models/Patient.cs
+++ CertiP3backingServ/Models/Patient.cs
@@ -5,21 +5,16 @@
         public string CI { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
-        public string BloodType { get; set; }
+      
 
         public Patient(string ci, string name, string lastName)
         {
             CI = ci;
             Name = name;
             LastName = lastName;
-            BloodType = RandomBloodType();
+          
         }
 
-        private string RandomBloodType()
-        {
-            string[] bloodTypes = { "A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-" };
-            Random random = new Random();
-            return bloodTypes[random.Next(bloodTypes.Length)];
-        }
+        
     }
 }
